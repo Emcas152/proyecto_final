3e50ec8335e3f4d134fb1124f296eed886d93fb9
Edwin Castillo <Edwincas1522@hotmail.com>
2021-06-08 15:22:56 -0600

    Agregados css fuentes y logo

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,13 @@
+import '../App.css';
 import React from 'react'
+import logo from '../images/Logo.png';
 
 export default function LandingPage() {
-    return (<h1>Landing Page Edwin Castillo</h1>)
+    return (
+        <div>
+            <h1>Landing Page Edwin Castillo</h1>
+            <img src={logo} alt="Logo" className={'App-logo'}/>
+        </div>
+    )
 
 }
